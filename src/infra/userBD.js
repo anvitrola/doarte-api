2065c7861736e52e698e7d2c79f247f7cf021c87
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-05 13:09:06 -0300

    Mocked databases for api test

---
+++ src/infra/userBD.js
@@ -0,0 +1,21 @@
+var bd = 
+[
+{id:"1",
+name:"Roberto",
+email:"roberto@email.com",
+password:"*******"},
+{id:"2",
+name:"Julia",
+email:"julia@email.com",
+password:"*******"},
+{id:"3",
+name:"Hugo",
+email:"hugo@email.com",
+password:"*******"},
+{id:"4",
+name:"Daniela",
+email:"dani@linda.com",
+password:"*******"}
+]
+
+module.exports = bd;
