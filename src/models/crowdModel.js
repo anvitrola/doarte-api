82d68cf232875c76ea085f58bd026decfa1de680
EliveltonSouzaDev <tst.elivelton@gmail.com>
2021-05-05 21:38:56 -0300

    change

--- src/models/crowdModel.js
+++ src/models/crowdModel.js
@@ -1,6 +1,5 @@
 class Crowdfunding {
-  constructor(id,id_user,title,category,value,deadline,creation_date,
-    description)
+  constructor(id,id_user,title,category,value,deadline,creation_date,description)
     {
       this.id = id,
       this.id_user = id_user,
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,13 @@
 class User {
+
   constructor(id, name, email, password) {
-    (this.id = id),
-      (this.name = name),
-      (this.email = email),
-      (this.password = password);
+
+          this.id = id,
+          this.name = name,
+          this.email = email,
+          this.password = password;
   }
+
 }
 
 module.exports = User;
