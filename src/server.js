0f6f8f2d486790da7d558965ec050f73a0205a97
anvitrola <vitoriavianathiss@outlook.com>
2021-05-06 17:23:38 -0300

    Finishing security config. database still has to be set up

--- src/server.js
+++ src/server.js
@@ -11,9 +11,8 @@ app.use(cors());
 
 await db.sequelize.sync({alter: true});
 
-app.get("/", (req, res) => {
-    res.send({"Doarte": "A Arte de Doar."});
-})
+require("./routes/auth.routes.js")(app);
+require("./routes/user.routes.js")(app);
 
 app.listen(port, (err) => {
     err ? console.log(err) : console.log(`[SUCCESS]: Server is running at http://localhost:${port}`);
