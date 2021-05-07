1c98e33e0cdf8709e1a6de8f36bfdffa394bac7a
Daniel Ferreira <danferreiradev@gmail.com>
2021-05-07 20:27:43 -0300

    refactor: controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,12 +2,11 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const db = require("../models");
-const config = require("../config/auth.config");
 const Users = db.Users;
 
 exports.signUp = (req, res) => {
     const {name, email, password} = req.body;
-    
+
     Users.create({
         email: email,
         name: name,
@@ -26,7 +25,7 @@ exports.signIn = (req, res) => {
         }
     })
     .then(user => {
-        if(!user) 
+        if(!user)
             res.status(404).send({message: "E-mail não registrado"});
 
         let isPasswordValid = bcrypt.compareSync(
@@ -44,12 +43,12 @@ exports.signIn = (req, res) => {
         let token = jwt.sign({id: user.id}, config.secret, {
             expiresIn: 43200 //12h
         });
-        
+
         //Aqui deve entrar tb a lista de projetos do usuário
         res.status(200).send({
             id: user.id,
             email: user.email,
-            acessToken: token 
+            acessToken: token
         });
     })
     .catch(err => res.status(500).send({message: err.message}))
