b1c8985991e38650e15e3276c77d61170bd8b767
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-11 14:49:20 -0300

    change verify to pick user ID

--- src/middleware/authJwt.js
+++ src/middleware/authJwt.js
@@ -4,13 +4,14 @@ const db = require("../models");
 const Users = db.Users;
 
 const verifyToken = (req, res, next) => {
-    let token = req.headers["x-acess-token"];
-
+    let token = req.headers["x-access-token"];
+    
     if(!token) res.status(403).send({message: "Conexão insegura."});
 
     jwt.verify(token, config.secret, (err, decoded) => {
         if(err) res.status(401).send({message: "Não autorizado"});
-        req.userId = decoded.id
+        res.locals.userId = decoded.id
+        
         next();
     })
 }
