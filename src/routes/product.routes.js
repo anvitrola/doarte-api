dfe476de839a5b998b23214877dee2c4bd30cd8d
anvitrola <vitoriavianathiss@outlook.com>
2021-05-14 00:36:38 -0300

    Organizing code

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -2,11 +2,8 @@ const products = require("../controllers/product.controller");
 const {authJwt} = require("../middleware");
 
 module.exports = app => {
-  
- 
-  
     // Create a new Tutorial
-  app.post("/api/test/product/create",authJwt, products.create);
+  app.post("/api/test/product/create",authJwt, products.createProduct);
   
     // Retrieve all products
   app.get("/api/test/product/findAll", products.findAll);
@@ -16,8 +13,6 @@ module.exports = app => {
   app.patch("/api/test/product/update/:id",authJwt, products.updateProduct);
   
   app.delete("/api/test/product/delete/:id",authJwt, products.deleteProduct);
-
-  
 
 };
   
