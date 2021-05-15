ca3ad9f2a48b66f6a0da38300338cebeff1882a6
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-15 18:25:29 -0300

    transaction implemented and working.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,159 +1,190 @@
 const db = require("../models");
 const bcrypt = require("bcryptjs");
-
-const Users = db.Users;
-const Product = db.Product;
+const Sequelize = require("sequelize");
+const { sequelize } = require("../models");
+const User = db.User;
+const Fundraiser = db.Fundraiser;
 
 const Op = db.Sequelize.Op;
 
 exports.allAccess = (_, res) => {
-
-    Users.findAll(
-        {
-            attributes: ['name', 'email']
-        }
-    )
-    .then(data => res.send(data))
-    .catch(err => {
-        res.status(500).send(
-            {
-                message: err.message || "Erro ao listar usuários."
-            }
-        );
+  User.findAll({
+    attributes: ["name", "email"],
+    where: { active: true },
+  })
+    .then((data) => res.send(data))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro ao listar usuários.",
+      });
     });
 };
 
 exports.userBoard = (req, res) => {
-    const {email} = req.body;
+  const { email } = req.body;
 
-    Users.findAll(
-        {
-            where: {email: email}
-        }
-    )
-    .then(data => res.send(data))
-    .catch(err => {
-        res.status(500).send(
-            {
-                message: err.message || "Erro ao requisitar informações do usuário."
-            }
-        );
+  User.findAll({
+    where: { email: email, active: true },
+  })
+    .then((data) => res.send(data))
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Erro ao requisitar informações do usuário.",
+      });
     });
 };
 
 exports.updateUser = async (req, res) => {
-    const id = res.locals.userId;
-    const {name, email, password} = req.body;
+  const id = req.userId;
+  const { name, email, password } = req.body;
 
-    await Users.update(
-        {
-            email: email,
-            name: name,
-            password: bcrypt.hashSync(password, 8)
-        }, 
-        {
-            where: {id: id}
-        }
-    )
-    .then(num => {
-        if (num == 1) {
-            res.send(
-                {
-                    message: "Usuário atualizado com sucesso."
-                }
-            );
-        } else {
-            res.send(
-                {
-                    message: "Não foi possível atualizar o usuário. Tente novamente."
-                }
-            );
-        }
+  await User.update(
+    {
+      email: email,
+      name: name,
+      password: bcrypt.hashSync(password, 8)
+    },
+    {
+      where: { id: id },
+    }
+  )
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Usuário atualizado com sucesso.",
+        });
+      } else {
+        res.send({
+          message: "Não foi possível atualizar o usuário. Tente novamente.",
+        });
+      }
     })
-    .catch(err => {
-        res.status(500).send(
-            {
-                message: "Erro na atualização do usuário. Tente novamente."
-            }
-        );
+    .catch((err) => {
+      res.status(500).send({
+        message: "Erro na atualização do usuário. Tente novamente.",
+      });
     });
 };
 
 exports.deleteUser = async (req, res) => {
-    const id = res.locals.userId;
-   
-    await Users.destroy(
-        {
-            where: {id: id}
-        }
-    )
-    .then(num => {
-        if (num == 1) {
-            res.send(
-                {
-                    message: "Usuário deletado com sucesso."
-                }
-            );
-        } else {
-            res.send(
-                {
-                    message: "Não foi possível deletar o usuário. Tente novamente."
-                }
-            );
-        }
+  const id = req.userId;
+
+  await User.update(
+    {
+      email: "deletado",
+      name: "deletado",
+      password: "deletado",
+      active: false
+    },
+    {
+      where: { id: id },
+    }
+  )
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Usuário deletado com sucesso.",
+        });
+      } else {
+        res.send({
+          message: "Não foi possível deletar o usuário. Tente novamente.",
+        });
+      }
     })
-    .catch(err => {
-        res.status(500).send(
-            {
-                message: "Erro ao deletar o usuário. Tente novamente."
-            }
-        );
+    .catch((err) => {
+      res.status(500).send({
+        message: "Erro ao deletar o usuário. Tente novamente.",
+      });
     });
 };
 
-exports.donation = (req, res) => {
-    const {donation_value} = req.body;
-    const id = res.locals.userId;
+exports.donation = async (req, res) => {
+  const { donation_value } = req.body;
+  const user_id = req.userId;
+  const fundraiser_id = req.params.id;
+  let newAmount = 0;
 
-    if (donation_value < 25 || !donation_value) res.status(400).send("Doação não pode ser inferior a 25 moedas");
+  if (donation_value < 25 || !donation_value) {
+    res.status(400).send("Doação não pode ser inferior a 25 moedas");
+    return;
+  }
 
-    Users.findAll(
+  const data = await sequelize.transaction(async () => {
+    try {
+      const request = await User.findOne(
         {
-            attributes: ['amount_money']
+          attributes: ["amount_money", "id"],
+          where: { id: user_id }
         },
-        {
-            where: {id: id}
-        }
-    )
-    .then(amount_money => {
-        if (amount_money < donation_value || amount_money == 0)
-            res.status(400).send({message: "Saldo insuficiente."}); 
-
-        else if ((amount_money -= donation_value) < 0){
-            res.status(400).send({message: "Usuário não pode ficar negativado."});
-        }
-
-        //Aqui falta fazer as verificações da vaquinha antes de realizar de fato a transação. (checar modelo BPMN).
-        const newAmount = amount_money - donation_value;
-
-        Users.update(
-            {
-                amount_money: newAmount
-            }, 
-            {
-                where: {id: id}
-            }
-        );
-
-        //Aqui entraria o update da vaquinha com o novo valor e novas checagens: essa vaquinha, c essa transação, atinge seu objetivo?
         
-        res.send("Doação realizada com sucesso.")
-    })
-    .catch(err => {
-        res.status(500).send(
-            {
-                message: err.message || "Erro no processamento da doação."
-            }
-        );
-    });
-}
+      );
+      return(request);
+    } catch (e) {
+      res.send(e);
+    }
+  });
+  const {dataValues:{amount_money}} = data
+  if (amount_money < donation_value || amount_money == 0) {
+    res.status(400).send({ message: "Saldo insuficiente." });
+    return;
+  } else if (amount_money < donation_value) {
+    res.status(400).send({ message: "Usuário não pode ficar negativado." });
+    return;
+  } else {
+    newAmount = amount_money - donation_value;
+  }
+  //Aqui falta fazer as verificações da vaquinha antes de realizar de fato a transação. (checar modelo BPMN).
+  await Fundraiser.findAll({
+      where: { id: fundraiser_id, user_id: user_id },
+  }).then((data) => {
+      if (!data) {
+        res.status(400).send({ message: "Não é possivel doar para sua própria vaquinha." });
+        return;
+      }
+
+  });
+
+      await User.update(
+      {
+        amount_money: newAmount,
+      },
+      {
+        where: { id: user_id },
+      }
+    );
+    //Aqui entraria o update da vaquinha com o novo valor e novas checagens: essa vaquinha, c essa transação, atinge seu objetivo?
+      await Fundraiser.update(
+      {
+        current_value: Sequelize.literal(`current_value + ${donation_value}`),
+      },
+      {
+        where: {
+          id: fundraiser_id,
+        },
+      }
+    )
+      .then((num) => {
+        if (num == 1) {
+          res.send({
+            message: "Doação realizada com sucesso.",
+          });
+        } else {
+          res.send({
+            message: "Não foi possível atualizar a vaquinha. Tente novamente.",
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: "Erro na atualização da vaquinha. Tente novamente.",
+        });
+      });
+
+  //   // .catch(err => {
+  //   //     res.status(500).send(
+  //   //         {
+  //   //             message: err.message || "Erro no processamento da doação."
+  //   //         }
+  //   //     );
+  //   // });
+};
