ba02c40b091e4c45bfc6ef951f795e1e7795c609
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-22 14:12:30 -0300

    restore after deploy failed.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,27 +1,13 @@
 module.exports = {
-  // database: "doarte",
-  // user: "doarte",
-  // password: "doarte",
-  // host: "localhost",
-  // dialect: "postgres",
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000
-  // }
-
-  development: {
-    database: "doarte",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  test: {
-    database: "doarte",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  production: {
-    use_env_variable: "DATABASE_URL",
-  },
+  database: "doarte",
+  user: "doarte",
+  password: "doarte",
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
