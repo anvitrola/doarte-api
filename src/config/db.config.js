345413ce4ad26365c4bb5ab75aa940d6546c0e7a
RafaelVi <rafaelalmeida383@gmail.com>
2021-05-22 15:26:57 -0300

    change

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  database: "doarte",
-  user: "doarte",
-  password: "doarte",
-  host: "localhost",
+  database: "d1afn8mm2694lc",
+  user: "hwbxrkdkoihymz",
+  password: "0a5953cd501b8c6c9e4d7b63da21dbdbd7a5cc33a1985aabde18b746281c86d6",
+  host: "ec2-54-224-194-214.compute-1.amazonaws.com",
   dialect: "postgres",
   pool: {
     max: 5,
